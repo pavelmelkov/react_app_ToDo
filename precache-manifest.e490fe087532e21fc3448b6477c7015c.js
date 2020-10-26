1125eacf56b2f0b612340c2ebc8adea96c896227
Pavel <house.melkov@gmail.com>
2020-10-27 00:04:31 +0300

    Updates

---
+++ precache-manifest.e490fe087532e21fc3448b6477c7015c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3630a3092fb8763a04b28a2da4644b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbdf0fd0e3d23f6abc62",
+    "url": "/static/css/main.b5489b16.chunk.css"
+  },
+  {
+    "revision": "8917b9b1e198d4d7aad5",
+    "url": "/static/js/2.e783dc1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e783dc1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbdf0fd0e3d23f6abc62",
+    "url": "/static/js/main.04120051.chunk.js"
+  },
+  {
+    "revision": "84cf180873f811dc457e",
+    "url": "/static/js/runtime-main.c722e200.js"
+  }
+]);
